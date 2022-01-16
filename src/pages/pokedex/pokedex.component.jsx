53a531aa93e2df6d893c31eba688f63c89a83d80
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-16 00:03:48 -0500

    Realizando consultas a la API mediante Axios y por Async Functions

--- src/pages/pokedex/pokedex.component.jsx
+++ src/pages/pokedex/pokedex.component.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 import './pokedex.styles.css';
 
 const PokedexPage = () => {
+    const baseUri = process.env.REACT_APP_BASE_API_URL;
+    const imageUri = process.env.REACT_APP_IMAGE_API_URL;
+
+    const [pokemonData, setPokemonData] = useState([]);
+
+    const getPokemonData = async () => {
+        // Usando el método clásico
+        try {
+            let url = `${baseUri}?limit=800`
+            let res = await fetch(url);
+
+            let data = await res.json();
+            console.log(data.results);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const axiosPokemonData = () => {
+        // Usando Axios
+        axios.get(`${baseUri}?limit=900`).then((response) => {
+            if(response.status >= 200 && response.status < 300) {
+                const { results } = response.data;
+                let newPokemonData = [];
+
+                results.forEach((pokemon, index) => {
+                    index++;
+                    let pokemonObject = {
+                        id: index,
+                        url: `${imageUri}/${index}.png`,
+                        name: pokemon.name,
+                    }
+
+                    newPokemonData.push(pokemonObject);
+                });
+                
+                setPokemonData(newPokemonData);
+            }
+        });
+    }
+
+    useEffect(() => {
+        // getPokemonData();
+        axiosPokemonData(); 
+    }, []);
+
     return(
-        <div className="">
-            <h1>Pokedex</h1>
-        </div>
+        <Box>
+            {
+                pokemonData ? pokemonData.map((pokemon) => {
+                    return <h1>{pokemon.name}</h1>
+                }) 
+                :
+                <CircularProgress style={{ marginTop: 100 }} />
+            }
+        </Box>
     );
 }
 
