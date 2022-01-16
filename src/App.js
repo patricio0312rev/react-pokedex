c3ce52d426d444bfd169ea439deefd0f4a65dbdf
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 23:40:50 -0500

    Añadiendo estilos y componentes de material UI a nuestro Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PokedexPage from './pages/pokedex/pokedex.component';
+import Navigator from './components/navigator/navigator.component';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello</h1>
         <Router>
+          <Navigator />
+
           <Routes>
             <Route path="/" element={<PokedexPage />} />
           </Routes>
--- src/components/navigator/navigator.component.jsx
+++ src/components/navigator/navigator.component.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
 import './navigator.styles.css';
 
-const Navigator = () => {
+const useStyles = makeStyles((theme) => ({
+    AppBar: {
+        backgroundColor: 'black!important',
+    },
+    Link: {
+        textDecoration: 'none',
+    },
+    title: {
+        cursor: 'pointer',
+        color: 'white',
+    }
+}));
 
+const Navigator = () => {
+    const classes = useStyles();
+
+    return(
+        <AppBar className={classes.AppBar} position="fixed" variant="h6">
+            <Toolbar>
+                <Link to="/" className={classes.Link}>
+                    <Typography className={classes.title}>Pokedex</Typography>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Navigator;
